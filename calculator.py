61639f6a892889de250fe0694787bad498ff27f5
Sudeshna Dey <113450003+sudeshna999@users.noreply.github.com>
2025-08-09 14:56:36 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("Simple Calculator")
+
+num1 = st.number_input("Enter first number")
+num2 = st.number_input("Enter second number")
+
+operation = st.radio("Operation",["Add","Subtract","Multiplication","Divide"])
+
+result = None
+if st.button("Calculate"):
+    if operation == "Add":
+        result = num1 + num2
+    elif operation == "Subtract":
+        result = num1 + num2
+    elif operation == "Multiplication":
+        result = num1 + num2
+    elif operation == "Divide":
+        result = num1 + num2
+else:
+    result = None
+
+if result:
+    st.success(f"Result:{result}")
+
